9fa83ffbb1cfb2363382abdc32e6f56c50f41dd4
tarek <tarekshawky2016@gmail.com>
2025-06-13 01:26:08 +0300

    redirect url en when add domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/en',
+                permanent: false, // or true in production
+            },
+        ];
+    },
 };
 
 export default nextConfig;
