40ca870794ec53f43cc0ad396d795c9a85024f5f
tarek <tarekshawky2016@gmail.com>
2025-06-13 03:05:11 +0300

    resolve language and Add Logo

--- src/app/[locale]/components/NavBar.tsx
+++ src/app/[locale]/components/NavBar.tsx
@@ -1,14 +1,21 @@
 import Link from "next/link";
 import LanguageSwitcher from "./LanguageSwitcher";
-
+import Image from "next/image";
+import osta from "../../../../public/osta.jpg"
 export default function Navbar({ locale }: { locale: string }) {
     return (
         <nav className="flex justify-between items-center px-6 py-4 bg-white shadow">
             <div className="text-xl font-bold">
-                <Link href={`/${locale}`}>MyApp</Link>
+                <Link href={`/${locale}`} className="flex items-center gap-4">
+                    <Image src={osta} alt="Osta Logo" width={56} height={56} />
+                    <div>
+                        <h2 className="text-xs">OSTA Services | خدمات آسطا</h2>
+                        <p className="text-xs hidden md:block">صيانة منزلية متكاملة واحترافية بأسعار تنافسية وسرعة تنفيذ عالية في عجمان ودبي والشارقة</p>
+                    </div>
+                </Link>
             </div>
 
-            <div className="flex items-center gap-8">
+            <div className="hidden md:flex items-center gap-8">
                 <Link href={`/${locale}/about`} className="hover:underline">
                     About
                 </Link>
