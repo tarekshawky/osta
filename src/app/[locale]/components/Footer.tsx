34dadda95b501d50ffbc453a881e2822353a65db
tarek <tarekshawky2016@gmail.com>
2025-07-08 14:31:00 +0400

    Add Icon Social Media in Footer

--- src/app/[locale]/components/Footer.tsx
+++ src/app/[locale]/components/Footer.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import ostaFooter from "../../../../public/logo-footer.png";
+import { FaLinkedin, FaFacebook, FaYoutube, FaInstagram } from 'react-icons/fa';
 
 export default function Footer() {
     return (
@@ -9,8 +10,19 @@ export default function Footer() {
                     <div>
                         <Image src={ostaFooter} alt="Osta Logo" width={100} height={51} className="w-64" />
                     </div>
-                    <div>
-                        Icons
+                    <div className="flex items-center gap-4">
+                        <a href="https://www.linkedin.com/company/osta-services/" target="_blank" rel="noopener noreferrer">
+                            <FaLinkedin />
+                        </a>
+                        <a href="https://www.facebook.com/ostaservice/" target="_blank" rel="noopener noreferrer">
+                            <FaFacebook />
+                        </a>
+                        <a href="https://www.youtube.com/@ostaservices/" target="_blank" rel="noopener noreferrer">
+                            <FaYoutube />
+                        </a>
+                        <a href="https://www.instagram.com/osta.services/" target="_blank" rel="noopener noreferrer">
+                            <FaInstagram />
+                        </a>
                     </div>
                 </div>
             </div>
